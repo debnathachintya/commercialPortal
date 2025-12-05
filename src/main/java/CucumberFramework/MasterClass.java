c1153ae0a85ffc4fcb7f064540ee19cbffc6ac95
Achintya Debnath <44962867+debnathachintya@users.noreply.github.com>
2025-12-05 23:35:21 +0530

    Update MasterClass.java

--- src/main/java/CucumberFramework/MasterClass.java
+++ src/main/java/CucumberFramework/MasterClass.java
@@ -2,6 +2,6 @@ package CucumberFramework;
 
 public class MasterClass {
     public static void main(String[] args) {
-        System.out.println("Test Message - Main");
+        System.out.println("Test Message - FJD Master Branch");
     }
 }
