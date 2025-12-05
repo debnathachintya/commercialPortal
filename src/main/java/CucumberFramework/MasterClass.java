7db05a2bba657e156a1f03f1e3a7159e3b4baa7a
debnathachintya <achintya.k.debnath@gmail.com>
2025-12-05 23:37:44 +0530

    5-Dec Commit_3

--- src/main/java/CucumberFramework/MasterClass.java
+++ src/main/java/CucumberFramework/MasterClass.java
@@ -3,9 +3,12 @@ package CucumberFramework;
 public class MasterClass {
     public static void main(String[] args) {
         System.out.println("Test Message - Main");
+        getMessage();
+        getMessage();
+        getMessage();
     }
 
-    public void getMessage() {
+    public static void getMessage() {
         System.out.println("Test");
     }
 }
